ee82409dcac6e67570716c45c9d88af5e3050c31
Ben-41 <wenyufanghk@gmail.com>
2023-09-20 11:01:54 +0800

    statistical analysis

---
+++ u_test.py
@@ -0,0 +1,157 @@
+import csv
+from scipy.stats import mannwhitneyu
+import pandas as pd
+import numpy as np
+
+def read_csv_file(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        rows = list(reader)
+    return rows
+
+def read_txt_file(filename):
+    samplelist = []
+    with open(filename,'r') as file:
+        lines = file.readlines()
+        lines = [line.rstrip('\n') for line in lines]
+        for line in lines:
+            samplelist.append(line)
+    return samplelist
+
+def get_sample_groups(rows,sample_list):
+    sample_groups = {'control': [], 'experimental': []}
+    sample_names = rows[0][1:]
+
+    for row in rows[1:]:
+        pathway_name = row[0]
+        for i, sample_value in enumerate(row[1:]):
+            sample_name = sample_names[i]
+            if sample_name in sample_list:
+                if sample_name.endswith('_1a'):
+                    sample_groups['control'].append((pathway_name, sample_name, sample_value))
+                elif sample_name.endswith('_3a'):
+                    sample_groups['experimental'].append((pathway_name, sample_name, sample_value))
+                elif sample_name.endswith('_1b'):
+                    sample_groups['control'].append((pathway_name, sample_name, sample_value))
+                elif sample_name.endswith('_3b'):
+                    sample_groups['experimental'].append((pathway_name, sample_name, sample_value))
+                elif sample_name.endswith('_3c'):
+                    sample_groups['experimental'].append((pathway_name, sample_name, sample_value))
+    # print(len(sample_groups["experimental"]))
+
+    return sample_groups
+
+def FC(group1,group2):
+    global fold_change
+    control_group = np.array([group1])
+    experimental_group = np.array([group2])
+    control_group_mean = np.mean(control_group)
+    experimental_group_mean = np.mean(experimental_group)
+
+    if experimental_group_mean != 0 and control_group_mean != 0:
+        fold_change = np.log2(experimental_group_mean / control_group_mean)
+    elif experimental_group_mean == 0 and control_group_mean != 0:
+        experimental_group_mean = 1e-8
+        fold_change = np.log2(experimental_group_mean / control_group_mean)
+    elif control_group_mean == 0 and experimental_group_mean != 0:
+        control_group_mean = 1e-8
+        fold_change = np.log2(experimental_group_mean / control_group_mean)
+    elif control_group_mean == 0 and experimental_group_mean == 0:
+        control_group_mean = 1e-8
+        experimental_group_mean = 1e-8
+        fold_change = np.log2(experimental_group_mean / control_group_mean)
+
+    return fold_change
+
+def U_test(group1,group2):
+    statistic, p_value = mannwhitneyu(group1, group2, alternative='two-sided')
+    return p_value
+
+if __name__ == "__main__":
+    # 读取CSV文件
+    filename = '/Users/ben/Documents/CDD/IBS/humann3_out/FC_unstratified_DATA.csv'  # 替换为你的CSV文件路径
+    data = read_csv_file(filename)
+
+    # 获取样本列表
+    sample_list = read_txt_file("/Users/ben/Documents/CDD/IBS/humann3_out/FC_treatment_list.txt")
+
+    # 获取样本分组
+    sample_groups = get_sample_groups(data,sample_list)
+
+    # 获得每组每个pathway的数据
+
+    pathway_value_control = {}
+    pathway_value_experimental = {}
+
+    k = []
+    j =[]
+    for group in sample_groups["control"]:
+        if group[1] in sample_list and group[1] not in k:
+            k.append(group[1])
+    print(len(k))
+    print(k)
+
+    for group in sample_groups["experimental"]:
+        if group[1] in sample_list and group[1] not in j:
+            j.append(group[1])
+    print(len(j))
+    print(j)
+
+    for group in sample_groups["control"]:
+        if group[1] in sample_list:
+            if group[0] in pathway_value_control:
+                pathway_value_control[group[0]].append(float(group[2]))
+            else:
+                pathway_value_control[group[0]] = []
+                pathway_value_control[group[0]].append(float(group[2]))
+
+    for group in sample_groups["experimental"]:
+        if group[1] in sample_list:
+            if group[0] in pathway_value_experimental:
+                pathway_value_experimental[group[0]].append(float(group[2]))
+            else:
+                pathway_value_experimental[group[0]] = []
+                pathway_value_experimental[group[0]].append(float(group[2]))
+
+
+    # 计算P-value 和 计算Fold-changes
+    result_lists = []
+    for pathway in pathway_value_control.keys():
+        result_list = []
+        pvalue = U_test(pathway_value_control[pathway], pathway_value_experimental[pathway])
+        fold_change = FC(pathway_value_control[pathway],pathway_value_experimental[pathway])
+        result_list.append(pathway)
+        result_list.append(pvalue)
+        result_list.append(fold_change)
+        result_lists.append(result_list)
+    # print(result_lists)
+    #导出
+    df = pd.DataFrame(result_lists)
+    df.to_csv("/Users/ben/Documents/CDD/IBS/humann3_out/u-test.csv")
+
+
+
+    for pathway in pathway_value_control.keys():
+        p_value = U_test(pathway_value_control[pathway], pathway_value_experimental[pathway])
+
+    sample_experimental = []
+    sample_control = []
+    for sample in sample_groups["experimental"]:
+        if sample[1] not in sample_experimental:
+            sample_experimental.append(sample)
+
+    for sample in sample_groups["control"]:
+        if sample[1] not in sample_control:
+            sample_control.append(sample)
+
+    # print("Experimental sample: ",len(sample_experimental))
+    # print("Control sample: ", len(sample_control))
+    # # 打印结果
+    # print('对照组:')
+    # for pathway_name, sample_name, sample_value in sample_groups['control']:
+    #     print('Pathway:', pathway_name, 'Sample:', sample_name, 'Value:', sample_value)
+    #
+    # print('实验组:')
+    # for pathway_name, sample_name, sample_value in sample_groups['experimental']:
+    #     print('Pathway:', pathway_name, 'Sample:', sample_name, 'Value:', sample_value)
+
---
+++ volcano_plot.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from adjustText import adjust_text
+import pandas as pd
+import numpy as np
+# 假设有两个列表，存储了基因的折叠变化（fold change）和p-value值
+
+def read_csv_file(filename,col):
+    df = pd.read_csv(filename)
+    Column_data = df.iloc[:,col]
+    return Column_data
+
+pathway_names = read_csv_file("/Users/ben/Documents/CDD/IBS/humann3_out/火山图-FC-FDR.csv",0)
+fold_change = read_csv_file("/Users/ben/Documents/CDD/IBS/humann3_out/火山图-FC-FDR.csv",1)
+p_values = read_csv_file("/Users/ben/Documents/CDD/IBS/humann3_out/火山图-FC-FDR.csv",2)
+
+# 创建一个数据框
+data = {'Fold Change': fold_change, 'p-value': p_values}
+df = pd.DataFrame(data)
+
+# 设置阈值
+fold_change_threshold = 1
+p_value_threshold = 0.05
+
+# 根据阈值筛选显著差异的基因
+significantly_upregulated = df[(df['Fold Change'] > fold_change_threshold) & (df['p-value'] < p_value_threshold)]
+significantly_downregulated = df[(df['Fold Change'] < -fold_change_threshold) & (df['p-value'] < p_value_threshold)]
+
+# 绘制火山图
+plt.figure(figsize=(10, 6))
+plt.scatter(df['Fold Change'], -np.log10(df['p-value']), color='gray', alpha=0.5)
+plt.scatter(significantly_upregulated['Fold Change'], -np.log10(significantly_upregulated['p-value']), color='red', label='Upregulated')
+plt.scatter(significantly_downregulated['Fold Change'], -np.log10(significantly_downregulated['p-value']), color='blue', label='Downregulated')
+plt.axvline(x=fold_change_threshold, color='gray', linestyle='--')
+plt.axvline(x=-fold_change_threshold, color='gray', linestyle='--')
+plt.axhline(y=-np.log10(p_value_threshold), color='gray', linestyle='--')
+plt.xlabel('log2(Fold Change)')
+plt.ylabel('-log10(p-value)')
+# plt.title('Volcano Plot')
+
+# 为显著变化的基因添加标签
+texts = []
+for i in significantly_upregulated.index:
+    texts.append(plt.text(significantly_upregulated.loc[i, 'Fold Change'],
+                          -np.log10(significantly_upregulated.loc[i, 'p-value']),
+                          pathway_names[i], color='red'))
+for i in significantly_downregulated.index:
+    texts.append(plt.text(significantly_downregulated.loc[i, 'Fold Change'],
+                          -np.log10(significantly_downregulated.loc[i, 'p-value']),
+                          pathway_names[i], color='blue'))
+
+# 调整标签位置，避免重叠
+adjust_text(texts, arrowprops=dict(arrowstyle='->', color='gray'))
+
+plt.legend(loc='lower right')
+plt.savefig("/Users/ben/Documents/CDD/IBS/humann3_out/火山图.png",dpi=300)
+plt.show()
